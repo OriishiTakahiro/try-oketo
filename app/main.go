7d563fbca6d4ddb41e9306b656871efc15314b1b
OriishiTakahiro <>
2022-09-21 00:56:59 +0900

    initial commit
    Signed-off-by: OriishiTakahiro <>

---
+++ app/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+var (
+	podIP = ""
+)
+
+type (
+	ReqBody struct {
+		Date string
+	}
+)
+
+func main() {
+	podIP = os.Getenv("POD_IP")
+
+	// echo instance
+	e := echo.New()
+
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ok")
+	})
+
+	e.GET("/ip", func(c echo.Context) error {
+		return c.String(http.StatusOK, podIP)
+	})
+
+	// start Server
+	e.Logger.Fatal(e.Start(":80"))
+}
